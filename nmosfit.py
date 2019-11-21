66855e9fffb6597d34c2a528002832de85b70081
Marcos Vallina <marcos@neumann.naninet>
2019-11-21 21:15:03 +0100

    Committer: Marcos Vallina <marcos@neumann.naninet>
     On branch dev
     Your branch is up to date with 'origin/dev'.
    
     Changes to be committed:
    	modified:   nmosfit.py
    	new file:   nmosfit2.py

--- nmosfit.py
+++ nmosfit.py
@@ -3,12 +3,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
+from scipy.optimize import curve_fit
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=str, help="data file")
 parser.add_argument("-R", "--rd", type=float, default=1e3, help="resistor on drain")
-parser.add_argument("-D", "--diagnose", action="store_true", help="plot aux polynomial")
-parser.add_argument("-I", "--init", type=float, default=10, help="iterative seed")
 parser.add_argument("-G", "--gain", type=float, default=25, help="gain desired (dB)")
 parser.add_argument("-P", "--plot", action="store_true", help="plot fitted polynomial")
 
@@ -22,54 +21,15 @@ except OSError:
     
 vgs_data, id_data = data[:, 0], data[:, 1]
 
-if args.diagnose:
-    vx = np.linspace(0, 10, 1000)
-    vy1 = [((vgs_data - v) @ id_data - 
-        np.square(vgs_data - v) @ id_data * np.sum(np.power(vgs_data - v, 3)) / 
-        np.sum(np.power(vgs_data - v, 4)))
-        for v in vx]
-    #vy2 = [((vgs_data @ id_data - 
-    #    np.square(vgs_data - v) @ id_data * np.sum(np.power(vgs_data - v, 3)) / 
-    #    np.sum(np.power(vgs_data - v, 4))) / np.sum(id_data))
-    #    for v in vx]
-    #vy3 = [(vgs_data @ id_data - np.sum(id_data) * v +
-    #    np.square(vgs_data - v) @ id_data * np.sum(np.power(vgs_data - v, 3)) / 
-    #    np.sum(np.power(vgs_data - v, 4)))]
-        
-    plt.plot(vx, vy1, color="r") 
-    #plt.plot(vx, vy2, color="g") 
-    #plt.plot(vx, vy3, color="b") 
-    plt.xlabel(r"$v_t$")
-    plt.ylabel(r"$p(v_t$)")
-    plt.grid()
-    plt.show()
-    exit()
-    
-# Biseccion
-va, vt = 0, 0
-vb = args.init
-while np.abs(va - vb) > 10 * np.finfo(float).eps * va:
-    vt = (va + vb) / 2
-    d = ((vgs_data - vt) @ id_data - 
-        (np.square(vgs_data - vt) @ id_data / np.sum(np.power(vgs_data - vt, 4))) *
-        np.sum(np.power(vgs_data - vt, 3))) 
-    if d > 0:
-        vb = vt
-    elif d < 0:
-        va = vt
-    else:
-        va = vb
+def idrain(vgs, k, vt):
+    return k * (vgs - vt) ** 2
 
-k = id_data @ np.square(vgs_data - vt) / np.sum(np.power(vgs_data - vt, 4))
-k2 = id_data @ (vgs_data - vt) / np.sum(np.power(vgs_data - vt, 3))
-
-if np.abs(k - k2) > 10 * np.finfo(float).eps * k:
-    print("fit invalid, try a different seed estimated from aux polynomial")
-    exit()
+popt, pcov = curve_fit(idrain, vgs_data, id_data)
+k, vt = tuple(popt)
 
 gain_target = 10 ** (args.gain / 20)
 vgsq = gain_target / (2 * k * args.rd) + vt
-idq = k * (vgsq - vt) ** 2
+idq = idrain(vgsq, k, vt)
 
 print("k = {:3.3f} mA/V^2".format(k * 1000))
 print("Vt = {:2.3f} V\n".format(vt))
