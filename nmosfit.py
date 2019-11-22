d611e055edd45bd230e3e2ef6aa3755944d5cc2c
Marcos Vallina <Marcos Vallina>
2019-11-22 01:03:31 +0100

    Committer: Marcos Vallina <marcos@neumann.naninet>
     On branch dev
     Your branch is up to date with 'origin/dev'.
    
     Changes to be committed:
    	modified:   nmosfit.py
    
     Changes to be committed:
    	modified:   nmosfit.py

--- nmosfit.py
+++ nmosfit.py
@@ -7,8 +7,9 @@ from scipy.optimize import curve_fit
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=str, help="data file")
-parser.add_argument("-R", "--rd", type=float, default=1e3, help="resistor on drain")
-parser.add_argument("-G", "--gain", type=float, default=25, help="gain desired (dB)")
+parser.add_argument("-Rd", "--rd", type=int, default=100, help="drain resistor")
+parser.add_argument("-Rs", "--rs", type=int, default=0, help="source resistor")
+parser.add_argument("-G", "--gain", type=float, default=25, help="gain (dB)")
 parser.add_argument("-P", "--plot", action="store_true", help="plot fitted polynomial")
 
 args = parser.parse_args()
@@ -30,19 +31,22 @@ k, vt = tuple(popt)
 gain_target = 10 ** (args.gain / 20)
 vgsq = gain_target / (2 * k * args.rd) + vt
 idq = idrain(vgsq, k, vt)
+vgq = vgsq + idq * args.rs
 
-print("k = {:3.3f} mA/V^2".format(k * 1000))
-print("Vt = {:2.3f} V\n".format(vt))
-print("Gain = {} dB".format(args.gain))
+print("k = {:6.1f} mA/V^2".format(k * 1000))
+print("Vt = {:6.3f} V\n".format(vt))
+print("Gain = {:5.1f} dB".format(args.gain))
 print("Rd = {} ohm".format(args.rd))
-print("Vgsq = {:3.3} V".format(vgsq))
-print("Idq = {:2.4} mA".format(1000 * idq))
+print("Rs = {} ohm".format(args.rs))
+print("Vgq = {:6.3f} V".format(vgq))
+print("Vgsq = {:6.3f} V".format(vgsq))
+print("Idq = {:6.1f} mA".format(1000 * idq))
 
 if args.plot:
     vgs = np.linspace(vgs_data[0], vgs_data[len(vgs_data) - 1], 1000)
     i_d = k * np.square(vgs - vt)
     
-    plt.title(r"$K = {0:3.3f} mA/V^2, V_t = {1:2.3f}V$".format(k * 1000, vt))
+    plt.title(r"$K = {0:6.1f} mA/V^2, V_t = {1:6.3f}V$".format(k * 1000, vt))
     plt.xlabel(r"$v_{gs}$")
     plt.ylabel(r"$i_d$")
     plt.grid()
