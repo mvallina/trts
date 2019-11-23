8e278711eac2221a58dc96bcd99dacb74910c9ff
Marcos Vallina <bourbakind@gmail.com>
2019-11-23 19:53:27 +0100

    En la rama dev  Cambios a ser confirmados: 	modificado:     nFETen.py

--- nFETen.py
+++ nFETen.py
@@ -1,5 +1,15 @@
 # FET acumulación canal N
 
-class nFETen:
-    def __init__(vgs_data, id_data)
+import numpy as np
+from scipy.optimize import curve_fit
+from resistor import best_rdiv, nearest_r, r_str
 
+class nFETen:
+
+    def __init__(self, vgs_data, id_data):
+        popt, pcov = curve_fit(idrain, vgs_data, id_data)
+        self.k, self.vt = tuple(popt)
+
+    @staticmethod
+    def idrain(vgs, k, vt):
+        return k * (vgs - vt) ** 2
