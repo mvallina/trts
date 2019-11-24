06ee0f5b4ae6230f219f71e55b87316d7411647d
Marcos Vallina <bourbakind@gmail.com>
2019-11-24 17:37:18 +0100

    En la rama dev  Cambios a ser confirmados: 	renombrado:     FETen.py -> feten.py

--- feten.py
+++ feten.py
@@ -2,23 +2,23 @@
 
 import numpy as np
 from scipy.optimize import curve_fit
-from resistor import best_rdiv, nearest_r, r_str
 
 class FETen:
 
-    def __init__(self, k, vt):
+    def __init__(self, k, vt, pcov):
         self.k = k
         self.vt = vt
+        self.dk, self.dvt = None, None if pcov == None else np.sqrt(np.diag(pcov))
 
     @classmethod
     def fromdata(cls, vgs_data, id_data):
         popt, pcov = curve_fit(idrain, vgs_data, id_data)
         k, vt = tuple(popt)
-        return cls(k, vt)
+        return cls(k, vt, pcov)
 
     @classmethod
     def fromparams(cls, k, vt):
-        return cls(k, vt)
+        return cls(k, vt, None)
 
     @staticmethod
     def idrain(vgs, k, vt):
