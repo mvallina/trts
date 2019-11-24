11b414adc26b5f417d8382a9689551abd343e3d4
Marcos Vallina <bourbakind@gmail.com>
2019-11-24 02:37:55 +0100

    En la rama dev  Cambios a ser confirmados: 	renombrado:     nFETen.py -> FETen.py 	renombrado:     etapa_csen.py -> etapase.py

--- FETen.py
+++ FETen.py
@@ -4,11 +4,21 @@ import numpy as np
 from scipy.optimize import curve_fit
 from resistor import best_rdiv, nearest_r, r_str
 
-class nFETen:
+class FETen:
 
-    def __init__(self, vgs_data, id_data):
+    def __init__(self, k, vt):
+        self.k = k
+        self.vt = vt
+
+    @classmethod
+    def fromdata(cls, vgs_data, id_data):
         popt, pcov = curve_fit(idrain, vgs_data, id_data)
-        self.k, self.vt = tuple(popt)
+        k, vt = tuple(popt)
+        return cls(k, vt)
+
+    @classmethod
+    def fromparams(cls, k, vt):
+        return cls(k, vt)
 
     @staticmethod
     def idrain(vgs, k, vt):
--- etapase.py
+++ etapase.py
@@ -1,3 +1,3 @@
 # Etapa surtidor común con FET de acumulación canal N
 
-class etapa_sen:
+class EtapaSE: 
